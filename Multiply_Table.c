1781895bf9ed828ee5a96ec23322055af4ade4a6
Nour Awawdeh <117414786+NOUR-AWAWDEH@users.noreply.github.com>
2022-11-04 03:46:42 +0300

    Add files via upload

---
+++ Multiply_Table.c
@@ -0,0 +1,26 @@
+//This was a Test in lecture : Multiply table
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+int main() {
+	int i,j;
+
+
+		for (i = 1; i <= 10;i++) {
+
+
+
+				for (j = 1; j <= 10;j++) {
+
+					printf("%3d   ", i * j);
+
+				}
+
+		    printf("\n");
+
+
+		}
+
+
+	printf("\n");
+	return 0;
+}
